e7087e29ad28e9ab639f05d46a816ea8f6c38cb6
brandonmbrito19 <brandonmbrito1995@alu.ufc.br>
2021-01-08 00:32:15 +0000

    Está dizendo que é erro de sintaxe, porém não estou conseguindo visualizar o erro.

--- main.py
+++ main.py
@@ -3,8 +3,33 @@
 
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
-    # COLOQUE SEU CÓDIGO AQUI
+    valor = float(input('Digite um valor entre 0.00 e 10.00: '))
 
+    total = valor
+    moeda = 1.00
+    totmoe = 0
+
+    while True:
+      if total >= moeda:
+        total -= moeda
+        totmoe +=  1
+    else
+      if totmoe > 0:
+        print('São necessárias: ')
+        print(f'Total de {totmoe} moedas de R${moeda}')
+      if moeda == 1.00:
+           moeda = 0.50
+      elif moeda == 0.50:
+           moeda = 0.25
+      elif moeda == 0.25:
+           moeda = 0.10
+      elif moeda == 0.10:
+           moeda = 0.05
+      elif moeda == 0.05:
+           moeda = 0.01
+           totmoe = 0
+      if total == 0:
+        break
 
 if __name__ == '__main__':
     main()
